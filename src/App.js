336d3d6cf07b5bdf5c4af70a4912b5cc61061b8e
skematt <matthewyang2002@hotmail.com>
2022-08-09 15:55:50 -0700

    Allowed adding feedbacks after pressing submit, with animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import {useState} from 'react'
 
 import Header from './components/Header'
@@ -9,6 +10,12 @@ import FeedbackData from './data/FeedbackData'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        console.log(newFeedback)
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
@@ -19,7 +26,7 @@ function App() {
         <>
             <Header />
             <div className='container'>
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback}
                 handleDelete={deleteFeedback}/>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useState} from 'react'
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
-function FeedbackForm(props) {
+function FeedbackForm({ handleAdd }) {
 
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -23,10 +25,23 @@ function FeedbackForm(props) {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
+            }
+            handleAdd(newFeedback)
+            setText('')
+        }
+    }
+
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
+                <RatingSelect select={(rating) => setRating(rating)}/>
                 <div className={"input-group"}>
                     <input onChange={handleTextChange}
                            type={'text'}
