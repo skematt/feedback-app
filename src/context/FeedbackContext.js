0bc008c8d994f2d726ec63b7b8ca2ba071b6a313
skematt <matthewyang2002@hotmail.com>
2022-08-17 14:52:19 -0700

    Added backend database

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,27 +1,30 @@
 import { v4 as uuidv4 } from 'uuid'
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 import FeedbackData from "../data/FeedbackData";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-    /*
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item is from context',
-            rating: 10
-        }
-    ])
-     */
-    const [feedback, setFeedback] = useState(FeedbackData)
+    const [isLoading, setisLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    useEffect(() => {
+        fetchFeedback()
+    },[])
+
+    // Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+        setFeedback(data)
+        setisLoading(false)
+    }
 
 
     // Add feedback
@@ -53,6 +56,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value={{
         feedback,
         feedbackEdit, // this is an object, not function
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
